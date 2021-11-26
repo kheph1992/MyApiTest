8b0fed6f5d5567d63c0ceb83471ed4bb31053d49
KANGA Francky Kouadio <legeniechimique@gmail.com>
2021-11-26 13:25:30 +0000

    RRW-1-Feature: Creating Car domain and controller

---
+++ WebUI/Features/Cars/CarController.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Features.Cars
+{
+    [Route("api/car")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Car>> GetCars()
+        {
+            var cars = new List<Car>();
+            var car1 = new Car
+            {
+                TeamName = "Team A",
+                Speed = 100,
+                MalfunctionChance = 0.2
+            };
+            var car2 = new Car
+            {
+                TeamName = "Team B",
+                Speed = 90,
+                MalfunctionChance = 0.15
+            };
+            cars.Add(car1);
+            cars.Add(car2);
+
+            return Ok(cars);
+        }
+
+    }
+}
